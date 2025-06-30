08d896be62db84c22892a196755b4ef6cc9a5239
Alexander Kosachev <kosachev_alex@mail.ru>
2025-06-30 21:36:21 +0300

    28

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{self, BufRead};
+use std::{
+    fs,
+    io::{self, BufRead},
+};
 
 use clap::Parser as CLAParser;
 
@@ -18,23 +21,24 @@ enum ArgumentsOutput {
 
 #[derive(CLAParser, Debug)]
 struct Arguments {
+    /// path
+    path: Option<String>,
     /// output
     #[arg(long, default_value = "text")]
     output: ArgumentsOutput,
 }
 
 fn main() {
-    let Arguments { output } = Arguments::parse();
+    let Arguments { path, output } = Arguments::parse();
 
     let mut parser = TreeSitterParser::new();
     parser
         .set_language(&TreeSitterLanguage::from(LANGUAGE))
         .unwrap();
 
-    let stdin = io::stdin();
-
-    for line in stdin.lock().lines() {
-        if let Ok(code) = line {
+    match path {
+        Some(path) => {
+            let code = fs::read_to_string(&path).unwrap();
             let tree = parser.parse(code.as_str(), None).unwrap();
 
             if let Ok(root) = flatten_tree(tree, code.as_str()) {
@@ -46,6 +50,27 @@ fn main() {
                         Ok(json) => println!("{}", json),
                         Err(e) => eprintln!("JSON serialization error: {}", e),
                     },
+                }
+            }
+        }
+        None => {
+            let stdin = io::stdin();
+
+            for line in stdin.lock().lines() {
+                if let Ok(code) = line {
+                    let tree = parser.parse(code.as_str(), None).unwrap();
+
+                    if let Ok(root) = flatten_tree(tree, code.as_str()) {
+                        match output {
+                            ArgumentsOutput::Text => {
+                                println!("{:#?}", root);
+                            }
+                            ArgumentsOutput::Json => match to_string(&root) {
+                                Ok(json) => println!("{}", json),
+                                Err(e) => eprintln!("JSON serialization error: {}", e),
+                            },
+                        }
+                    }
                 }
             }
         }
